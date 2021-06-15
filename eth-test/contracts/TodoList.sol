ccf396e5acd13dea69401c852a0d385f8011efda
Aleksandr Molchagin <contact@aleksandrmolchagin.com>
2021-06-15 18:15:59 +0300

    Built data structure

--- eth-test/contracts/TodoList.sol
+++ eth-test/contracts/TodoList.sol
@@ -4,4 +4,20 @@ contract TodoList {
 
     uint public taskCount = 0; //Stable Variable for Todo list counter. Keeps track of number of lists
 
+    struct Task {
+        uint id;
+        string content;
+        bool completed; 
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Say hello to Revaz");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] =  Task(taskCount, _content, false);
+    }
 }
