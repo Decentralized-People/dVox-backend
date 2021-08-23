cf9c09df705aedd5250068271272c95d8ef9335b
Revaz Bakuradze <60052562+RevazBakuradze@users.noreply.github.com>
2021-08-24 00:50:45 +0400

    uncommented votes

--- contracts/PostContract.sol
+++ contracts/PostContract.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+/*
+    Voting system was temporarily moved to firestore to optimize smart contract.
+    number of upvotes and downvotes are not sensitive information.
+*/
 contract PostContract {
 
-    uint public postCount = 0; 
-    
+    uint public postCount = 0;
+
     struct Comment {
         uint commentID;
         string commentAuthor;
@@ -18,6 +22,8 @@ contract PostContract {
         string author;
         string message;
         string hashtag;
+        //        int upVotes;
+        //        int downVotes;
         uint commentCount;
         bool ban;
         mapping (uint => Comment) comments;
@@ -29,7 +35,7 @@ contract PostContract {
         createPost("Welcome message","Creator", "Hello World!! This is our first post.", "hashtagtest");
         addComment(1, "Aleksandr", "Hello, this is the first comment!");
     }
- 
+
     function createPost(string memory _title, string memory _author, string memory _message, string memory _hashtag) public {
         postCount ++;
         Post storage post = posts[postCount];
@@ -38,16 +44,26 @@ contract PostContract {
         post.author = _author;
         post.message = _message;
         post.hashtag = _hashtag;
+        //        post.upVotes = 0;
+        //        post.downVotes = 0;
         post.commentCount = 0;
         post.ban = false;
     }
+
+    //    function upVote(uint id, int vote) public{
+    //        posts[id].upVotes = posts[id].upVotes + vote;
+    //    }
+    //
+    //    function downVote(uint id, int vote) public{
+    //        posts[id].downVotes = posts[id].downVotes + vote;
+    //    }
 
     function addComment(uint _postID, string memory _comment_author, string memory _comment_message) public {
         Post storage post = posts[_postID];
         post.commentCount++;
         post.comments[post.commentCount] = Comment({commentID: post.commentCount, commentAuthor:_comment_author, commentMessage: _comment_message, commentBan: false});
     }
-    
+
     function getComment(uint postID, uint commentID) public view returns(uint id, string memory author, string memory message, bool ban) {
         Comment storage comment = posts[postID].comments[commentID];
         id = comment.commentID;
